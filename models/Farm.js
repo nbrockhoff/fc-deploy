b5972a61408f819ed60658f73147d8adad81cfda
Noelle Brockhoff <noelle@nbrockhoff.com>
2018-01-03 13:30:19 -0600

    Been a while - should probably commit.

---
+++ models/Farm.js
@@ -0,0 +1,122 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const farmSlug = require('slugs');
+
+const farmSchema = new mongoose.Schema({
+	farmName: {
+		type: String,
+		trim: true,
+		required: 'Please enter a farm name.'
+	},
+	farmSlug: String,
+	farmDescription: {
+		type: String, 
+		trim: true
+	},
+	farmTags: [String],
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	farmLocation: {
+		type: {
+			type: String,
+			default: 'Point'
+		},
+		coordinates:  [{
+			type: Number, 
+			required: 'You must supply coordinates.'	
+		}],
+		address: {
+			type: String,
+			required: 'You must supply an address.'
+		}
+	},
+	farmPhoto: String,
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: 'You must supply an author'
+  }
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+});
+
+farmSchema.pre('save',async function(next){
+	if (!this.isModified('farmName')){
+		
+		next();
+		return;
+	}
+	this.farmSlug = farmSlug(this.farmName);
+	const slugRegEx = new RegExp(`^(${this.farmSlug})((-[0-9]*$)?)$`, 'i');
+	const farmsWithSlug = await this.constructor.find({ farmSlug: slugRegEx });
+	if(farmsWithSlug.length){
+		this.farmSlug = `${this.farmSlug}-${farmsWithSlug.length + 1}`;
+	} 
+
+	next();
+});
+
+farmSchema.statics.getFarmTagsList = function() {
+	return this.aggregate([
+		{ $unwind: '$farmTags' },
+		{ $group: {
+			_id: '$farmTags', count: { $sum: 1 }
+			}
+		},
+		{ $sort: {
+			count: -1
+			}	
+		}
+	]);
+}
+
+// Define our indexes
+farmSchema.index({
+  farmName: 'text',
+  farmDescription: 'text',
+  farmTags: 'text'
+});
+
+farmSchema.index({ farmLocation: '2dsphere' });
+
+
+farmSchema.statics.getTopFarms = function() {
+  return this.aggregate([
+    // Lookup farms and populate their reviews
+    { $lookup: { from: 'reviews', localField: '_id', foreignField: 'farm', as: 'reviews' }},
+    // filter for only items that have 2 or more reviews
+    { $match: { 'reviews.1': { $exists: true } } },
+    // Add the average reviews field
+    { $project: {
+      photo: '$$ROOT.farmPhoto',
+      name: '$$ROOT.farmName',
+      reviews: '$$ROOT.reviews',
+      slug: '$$ROOT.farmSlug',
+      averageRating: { $avg: '$reviews.rating' }
+    } },
+    // sort it by our new field, highest reviews first
+    { $sort: { averageRating: -1 }},
+    // limit to at most 10
+    { $limit: 10 }
+  ]);
+}
+
+// find reviews where the farms _id property === reviews farm property
+farmSchema.virtual('reviews', {
+  ref: 'Review', // what model to link?
+  localField: '_id', // which field on the farm?
+  foreignField: 'farm' // which field on the review?
+});
+
+function autopopulate(next) {
+  this.populate('reviews');
+  next();
+}
+
+farmSchema.pre('find', autopopulate);
+farmSchema.pre('findOne', autopopulate);
+
+module.exports = mongoose.model('Farm', farmSchema);
